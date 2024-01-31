c29db39cd25f4bb5f4968390d39c950f5a882531
proletarianwarrior <3394219182@qq.com>
2024-01-31 23:55:36 +0800

    全新版本

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from FeaturePointSelect import select_feature_points
 from PowerIntervalSelect import select_power_interval
 from DocReporter import create_doc
 
-file_path = Path('./data/tjh.txt')
-select_filter_param(file_path, method='butter')  # 滤波参数选择器
+file_path = Path('./data/ck.txt')
+# select_filter_param(file_path, method='butter')  # 滤波参数选择器
 # select_feature_points(file_path)  # 特征点选择器
 # select_power_interval(file_path)  # 动力反应区间选择器
-# create_doc(file_path)  # 生成绘图报告
+create_doc(file_path)  # 生成绘图报告
